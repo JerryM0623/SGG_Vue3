f917b8adb5828c43aad85e399ca3d433fe7f348f
mjw <jiaweima2000@163.com>
2023-06-04 21:15:53 +0800

    feat: feature 1 is ready

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+console.log("jhfik jnfkdewn mnf" +
+"klewn");
+
+const a = () => 
+{
+
+  const b = 1;
+  var c = 100;
+  let aaa = "nbfjek";
+
+}
+
+</script>
+
+<template>
+  <div>
+    <h1> 
+        App
+
+    </h1>
+  </div>
+</template>
+
+<style scope></style>
