03631b96e4608459aaef3dd24c5f0351aaffa3b4
mjw <jiaweima2000@163.com>
2023-06-09 02:14:06 +0800

    fix: 修正devtool不展示pinia入口的问题

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,20 @@ import router from './router'
 
 import pinia from './store'
 
-const app = createApp(App)
+/**
+ * 禁止使用
+ * const app = createApp(App)
+ * app.use(xxx)
+ * app.use(yyy)
+ * ...
+ * 的形式初始化Vue3
+ * 因为这种形式在开发者工具（V6.5.0）上会无法展示 router pinia 等插件
+ * 修改成如下形式便可重新激活上述插件的入口
+ */
 
-app.use(ElementPlus)
-app.use(gloablComponent)
-app.use(router)
-app.use(pinia)
-app.mount('#app')
+createApp(App)
+  .use(ElementPlus)
+  .use(gloablComponent)
+  .use(router)
+  .use(pinia)
+  .mount('#app')
