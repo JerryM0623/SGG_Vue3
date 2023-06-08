018cbb33105f7ab43d8adec156a1ab2ef14a3823
mjw <jiaweima2000@163.com>
2023-06-09 01:00:12 +0800

    feat: login页面静态编写完毕

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,115 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { User, Lock } from '@element-plus/icons-vue'
+import { ref, reactive } from 'vue'
+
+const loginLoading = ref(false)
+const loginFormRef = ref(null)
+
+const loginForm = reactive({
+  username: '',
+  password: '',
+})
+
+const rules = {
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 5, message: '用户名至少5个字符', trigger: 'blur' },
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    { min: 6, message: '密码至少6个字符', trigger: 'blur' },
+  ],
+}
+
+function login() {
+  console.log('login...')
+}
+</script>
 
 <template>
-  <div class="login-container container">
-    <h1>login页面</h1>
+  <div class="login-container">
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24" class="right-col">
+        <el-form
+          class="login-form"
+          :model="loginForm"
+          :rules="rules"
+          ref="loginFormRef"
+        >
+          <div class="title">Hello!</div>
+          <div class="sub-title">键入账户密码开始使用吧!</div>
+          <el-form-item prop="username">
+            <el-input
+              :prefix-icon="User"
+              placeholder="请输入用户名"
+              v-model="loginForm.username"
+            ></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input
+              :prefix-icon="Lock"
+              type="password"
+              placeholder="请输入密码"
+              show-password
+              v-model="loginForm.password"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button
+              class="login-btn"
+              type="primary"
+              @click="login"
+              :loading="loginLoading"
+            >
+              登录
+            </el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.login-container {
+  width: 100%;
+  height: 100vh;
+  background: url('@/assets/images/background.jpg') no-repeat;
+  background-size: cover;
+
+  .el-row {
+    height: 100%;
+
+    .right-col {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+
+      .login-form {
+        width: 70%;
+        background: url('@/assets/images/login_form.png');
+        background-size: cover;
+        padding: 20px;
+
+        .title {
+          font-size: 40px;
+          color: white;
+        }
+
+        .sub-title {
+          font-size: 20px;
+          color: white;
+          margin: 20px 0;
+        }
+
+        .login-btn {
+          width: 100%;
+        }
+      }
+    }
+  }
+}
+</style>
